ac7443b9a11a1814c162dccace587e9619b35744
Nilam Jadhav <99525537+nilamsjadhav@users.noreply.github.com>
2022-05-07 17:49:46 +0530

    Added program that will convert csv to markdown table.

---
+++ csvToObjects.js
@@ -0,0 +1,37 @@
+const fs  = require('fs');
+
+const readData = file => fs.readFileSync(file, 'utf8');
+
+const splitRecord = record => record.split('|');
+
+const keys = records => splitRecord(records);
+
+const determineValue = function (value) {
+  return value.includes(',') ? value.split(',') : value;
+};
+
+const assignValue = function (fields) {
+  return function (element, value, index) {
+    const key = fields[index];
+    let newValue = determineValue(value);
+    element[key] = +newValue || newValue;
+    return element;
+  }
+};
+
+const createObject = function (fields) {
+  return (record) => {
+    return record.reduce(assignValue(fields), {});
+  }
+};
+
+const convertToObject = function (records, fields) {
+  return records.map(splitRecord).map(createObject(fields));
+};
+
+const structureDetails = function (file) {
+  const records = readData(file).split('\n');
+  return convertToObject(records.slice(1), keys(records[0]));
+};
+
+console.log(structureDetails('./pokemonDetails.csv'));
---
+++ csvToTable.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+
+const readData = file => fs.readFileSync(file, 'utf8');
+
+const writeData = content => fs.writeFileSync('./index.md', content, 'utf8');
+
+const tableHeader = record => [row(record), separator(record)];
+
+const giveLength = record => record.split('|').length;
+
+const separator = (record) => '|--'.repeat(giveLength(record)) + '|\n';
+
+const row = record => '|' + record + '|\n';
+
+const table = (records) => records.slice(1).map(row);
+
+const structureDetails = function (file) {
+  const records = readData(file).split('\n');
+  const list = table(records)
+  list.unshift(...tableHeader(records[0]));
+  writeData(list.join(''));
+};
+
+console.log(structureDetails('./students.csv'));
